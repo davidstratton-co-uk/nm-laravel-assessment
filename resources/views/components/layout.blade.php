655baec5dce44e0cb34a7531f455cfe30cece104
David Stratton <me@davidstratton.co.uk>
2025-09-19 09:20:30 +0100

    Initital Repo

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NM Laravel Assessment | David Stratton</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+    <div>
+        <nav>
+            <div>
+                <a href="#">
+                    Laravel Assessment
+                </a>
+            </div>
+            <ul>
+                <li><a href="#">Companies</a></li>
+                <li><a href="#">Employees</a></li>
+            </ul>
+            <ul>
+                <li>
+                    <form action="/logout" method="POST">
+                        <a href="#">Logout</a>
+                    </form>
+                </li>
+            </ul>
+        </nav>
+
+        <main>
+            {{ $slot }}
+        </main>
+    <div>
+</body>
+</html>
